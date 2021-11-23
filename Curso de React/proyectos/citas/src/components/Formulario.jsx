2bea2cd03b27e1db6a7ab09fc3ade99c0b2f33cc
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-11-23 07:51:44 -0300

    Formulario de citas 2

--- Curso de React/proyectos/citas/src/components/Formulario.jsx
+++ Curso de React/proyectos/citas/src/components/Formulario.jsx
@@ -1,7 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 
 const Formulario = () => {
+    const [cita, actualizarCitas] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const actualizarState = (e) => {
+        actualizarCitas({
+            ...cita,
+            [e.target.name]:e.target.value
+        })
+    };
+const {mascota,propietario,fecha,hora,sintoma} = cita
     return (
         <Fragment>
             <h2>Crear Cita</h2>
@@ -9,33 +24,44 @@ const Formulario = () => {
                 <label>Nombre Mascota</label>
                 <input
                     type="text"
-                    neme="mascota"
+                    name="mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={actualizarState}
+                    value={mascota}
                 />
                 <label>Nombre del dueño</label>
                 <input
                     type="text"
-                    neme="propietario"
+                    name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del dueño de la mascota"
+                    onChange={actualizarState}
+                    value={propietario}
                 />
                 <label>Nombre Mascota</label>
                 <input
                     type="date"
-                    neme="mascota"
+                    name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value={fecha}
                 />
                 <label>Hora</label>
                 <input
                     type="time"
-                    neme="hora"
+                    name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value={hora}
                 />
                 <label>Síntomas</label>
-                <textarea className="u-full-width">
-
-                </textarea>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                    onChange={actualizarState}
+                    value={sintoma}
+                ></textarea>
                 <button className="u-full-width button-primary">Agregar cita</button>
             </form>
         </Fragment>
